a6735ce2bb17a5e375991fb1f9e7cd0cd6b46aa0
bryan <bryan.mrspro@gmail.com>
2024-12-23 23:19:13 +0100

    maj

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,9 @@
 import streamlit as st
-from PIL import Image
-import numpy as np
-import cv2
 
-st.title("Scanner de Code-Barres avec Streamlit et OpenCV")
-st.write("Utilisez la caméra de votre téléphone pour scanner un code-barres.")
+st.title("Test d'OpenCV")
 
-# Utiliser le widget camera_input pour capturer une image
-uploaded_image = st.camera_input("Prenez une photo du code-barres")
-
-def decode_qr(image):
-    # Convertir l'image PIL en format OpenCV
-    cv_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
-    qr_decoder = cv2.QRCodeDetector()
-    data, bbox, _ = qr_decoder.detectAndDecode(cv_image)
-    return data
-
-if uploaded_image is not None:
-    # Ouvrir l'image avec PIL
-    image = Image.open(uploaded_image)
-    st.image(image, caption='Image capturée', use_column_width=True)
-
-    # Décoder le code QR
-    code = decode_qr(image)
-
-    if code:
-        st.success(f"**Code QR détecté :** {code}")
-    else:
-        st.error("Aucun code QR détecté. Veuillez réessayer.")
+try:
+    import cv2
+    st.write(f"OpenCV version: {cv2.__version__}")
+except ImportError as e:
+    st.error(f"Erreur lors de l'importation de cv2: {e}")
